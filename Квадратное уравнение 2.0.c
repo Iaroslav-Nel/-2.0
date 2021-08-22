0e21c423ddf7c44a21854461c833a6ecd25527da
Iaroslav-Nel <89302587+Iaroslav-Nel@users.noreply.github.com>
2021-08-23 00:57:11 +0300

    Add files via upload

---
+++ Квадратное уравнение 2.0.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+main ()
+{
+    int a, b, c;
+
+    scanf ("%d %d %d", &a, &b, &c);
+
+    int d = sqrt( b * b - 4 * a * c );
+
+    if ( d < 0 )
+    {
+        printf ("no KOßN");
+    }
+    else
+    {
+        float x = ( -b + d ) / ( 2 * a );
+
+        float y = ( -b - d ) / ( 2 * a );
+
+        printf (" %.2f  %.2f ", x, y);
+    }
+
+}
