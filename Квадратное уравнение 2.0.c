9eaf2fd5f257ece0ca0b0d5c35f33975936a3e99
Iaroslav-Nel <89302587+Iaroslav-Nel@users.noreply.github.com>
2021-08-25 16:26:41 +0300

    Update Квадратное уравнение 2.0.c

--- Квадратное уравнение 2.0.c
+++ Квадратное уравнение 2.0.c
@@ -1,29 +1,83 @@
 #include <stdio.h>
 #include <math.h>
 
-main ()
+int Solve (double a, double b, double c, double* x1, double* x2);
+
+int main ()
 {
-    int a = 0;
-    
-    int b = 0;
-    
-    int c = 0;
+    double a = 0;
+    double b = 0;
+    double c = 0;
 
-    scanf ("%d %d %d", &a, &b, &c);
+    scanf ("%lg %lg %lg", &a, &b, &c);
 
-    int d = (b * b - 4 * a * c);
+    double x1 = 0;
+    double x2 = 0;
 
-    if (d < 0)
+    int Roots = Solve (a, b, c, &x1, &x2);
+
+    switch (Roots)
     {
-        printf ("no KOßN");
+
+    case 0: printf ("no_roots\n");
+
+            break;
+
+    case 1: printf ("x = %lg\n", x1);
+
+            break;
+
+    case 2: printf ("x1 = %lg, x2 = %lg\n", x1, x2);
+
+            break;
+    }
+}
+
+int Solve (double a, double b, double c, double* x1, double* x2)
+{
+
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+            {
+                printf ("you are stupid\n");
+            }
+            else
+            {
+                printf ("you are stupid too\n");
+            }
+        }
+        else
+        {
+            printf ("x = %lg", -c / b);
+        }
     }
     else
     {
-        float x = (-b + sqrt(d)) / (2 * a);
+        double d = b * b - 4 * a * c;
 
-        float y = (-b - sqrt(d)) / (2 * a);
+        if (d < 0)
+        {
+            printf ("d < 0\n");
+        }
 
-        printf (" %.2f  %.2f ", x, y);
+        else
+        {
+            if (d == 0)
+            {
+                *x1 = *x2 = -b / (2 * a);
+
+                printf ("x = %lg\n", *x1);
+            }
+            else
+            {
+                *x1 = (-b + sqrt(d)) / (2 * a);
+                *x2 = (-b - sqrt(d)) / (2 * a);
+
+                printf ("x1 = %lg, x2 = %lg\n", *x1, *x2);
+            }
+        }
     }
-
 }
